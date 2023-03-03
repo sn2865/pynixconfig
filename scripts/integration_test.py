711ebd6933cbe89a7e62d4d7fc9f4faf5bfea062
sergionahas <nahassergio14@gmail.com>
2023-03-02 19:56:55 -0500

    added integration test

--- scripts/integration_test.py
+++ scripts/integration_test.py
@@ -7,9 +7,21 @@ font = "bold"
 test_config = config(get_color_code(color), get_font_code(font))
 main(color, font)
 
-command = "echo $LS_COLORS"
-output = os.system(command)
-print(output)
+#command = "echo $LS_COLORS"
+#output = os.popen(command).read()
+#print(output)
 
-assert output == test_config
+config_format = "LS_COLORS=" + '"{}"'.format(test_config)
+
+with open(get_full_path('~/.bashrc')) as f:
+    for line in f:
+        pass
+    last_line = line
+
+if last_line == config_format:
+    print("TRUE")
+else:
+    print("test config: " + config_format)
+    print("echo output: " + last_line)
+    print("FALSE")
 
